adbb028c6d53e84ba417b084e476f2b53d38125c
Chris <kfb12157@uni.strath.ac.uk>
2016-12-22 00:13:16 +0000

    added preliminary structure for control loop

---
+++ overall_control_loop.py
@@ -0,0 +1,10 @@
+# Imports
+
+# def loop:
+	# get motor speed
+	# get motor control action (delta speed)
+	# apply new motor speed
+	# get pend value
+	# image recog do?
+	# record motor speed
+	# record pend value
