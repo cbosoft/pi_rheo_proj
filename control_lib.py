347f7e094e45bf607c13a4d74501568df947304f
Chris <kfb12157@uni.strath.ac.uk>
2016-12-25 23:10:51 +0000

    changed sample time method

--- control_lib.py
+++ control_lib.py
@@ -14,49 +14,50 @@ class PIDcontroller:
 	ca = [0,0,0]
 	e = [0,0,0]
 	
-	time_of_last = 0
-	sample_time = 0
+#	time_of_last = 0
+	sample_time = 0.1
 	
-	def __init__(self, KP_, KI_, KD_):
+	def __init__(self, KP_, KI_, KD_, sample_time_):
+		self.sample_time  sample_time_;
 		if (KP_ == 0):
-			P_On = False
+			self.P_On = False
 		else:
-			P_On = True
-			KP = KP_
+			self.P_On = True
+			self.KP = KP_
 		
 		if (KI_ == 0):
-			I_On = False
+			self.I_On = False
 		else:
-			I_On = True
-			KI = KI_
+			self.I_On = True
+			self.KI = KI_
 		
 		if (KD_ == 0):
-			D_On = False
+			self.D_On = False
 		else:
-			D_On = True
-			KD = KD_
+			self.D_On = True
+			self.KD = KD_
 	
-	def shift_arrs():
-		temp = ca[1]
-		ca[1] = ca[2]
-		ca[0] = temp
-		temp = e[1]
-		e[1] = e[2]
-		e[0] = temp
+	def shift_arrs(self):
+		temp = self.ca[1]
+		self.ca[1] = self.ca[2]
+		self.ca[0] = temp
+		temp = self.e[1]
+		self.e[1] = self.e[2]
+		self.e[0] = temp
 	
 	def get_control_action(self, u_val):
 		# Control action is a function of the k parameters, the previous control actions, the previous errors, and the sample time
 		
-		# Get actual time between samples (will vary, use an average?)
-		if (time_of_last == 0):
-			time_of_last = lambda: int(round(time.time() * 1000))
-			sample_time = 0.1
-		else:
-			sample_time = time_of_last - (lambda: int(round(time.time() * 1000)))
-			time_of_last = lambda: int(round(time.time() * 1000))
+		# Get actual time between samples (will vary, use an average?) NOPE
+#		if (time_of_last == 0):
+#			time_of_last = int(round(time.time() * 1000))
+#			sample_time = 0.1
+#		else:
+#			sample_time = (int(round(time.time() * 1000))) - time_of_last
+#			time_of_last = int(round(time.time() * 1000))
 		
 		# move stored values down
-		shift_arrs()
-		e[2] = set_point - u_val
-		ca[2] = ca[1] + KP * (e[2] - e[1]) + KI * sample_time * e[2]
+		self.shift_arrs()
+		self.e[2] = self.set_point - u_val
+		self.ca[2] = self.ca[1] + self.KP * (self.e[2] - self.e[1]) + self.KI * self.sample_time * self.e[2]
 		return ca[2]
