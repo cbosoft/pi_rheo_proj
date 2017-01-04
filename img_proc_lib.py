6cb4bf41433a73f72665592a90c3c48557bbcd17
Chris <kfb12157@uni.strath.ac.uk>
2017-01-04 01:50:46 +0000

    fixins'

--- img_proc_lib.py
+++ img_proc_lib.py
@@ -15,31 +15,33 @@ from PIL import Image # uses python imaging library (PIL v1.1.7 for python 2.7)
 
 # Image_Processor class will take an image
 class Image_Processor(object):
-	img = Image.New("RGB",[10,10],[0,0,0])					# image object used by the methods in this class
+	img = Image.new("RGB",(10,10),(0,0,0))					# image object used by the methods in this class
 	pix = img.load()										# pixel data for the image
 	img_edge = img											# edge detected image
-	pix_edge = img_edge.Load()								# pixel data for the edge detected image
+	pix_edge = img_edge.load()								# pixel data for the edge detected image
 	crack_found = False										# has a crack been identified in the image?
+	verbose = False
 	
 	# initialisation method
 	def __init__(self):
-		self.img = Image.New("RGB",[10,10],[0,0,0])			# creates a temporary blank small image
+		self.img = Image.new("RGB",(10,10),(0,0,0))			# creates a temporary blank small image
 		self.pix = self.img.load()							# gets pixel data
 		self.img_edge = self.img							# creates a placeholder for the edge detected image
-		self.pix_edge = self.img_edge.Load()				# and gets image data for the edge detected image
+		self.pix_edge = self.img_edge.load()				# and gets image data for the edge detected image
 		self.crack_found = False							# initially no cracks found
 	
 	# sets new image in processor
 	def set_img(self, path):
 		self.img = Image.open(path)
-		self.pix = img.Load()
-		self.img_edge = edge_det()
-		self.pix_edge = img_edge.Load()
+		self.pix = self.img.load()
+		#self.img_edge = self.edge_det()
+		#self.pix_edge = self.img_edge.load()
 		self.crack_found = False
 	
 	# apply edge detection algorithm
-	def edge_det(self, image=self.img, BGT=245, crack_thresh=5):
-															# set up working variables
+	def edge_det(self, BGT=245, crack_thresh=5):
+                self.verb_prnt("Edge detection begninning...")
+		image = self.img													# set up working variables
 		pixels = image.load()								# get image pixel data
 		image_edge = image									# create edge image the same size as the working image
 		pixels_edge = image_edge.load()						# create handler for edge detected image's pixel data
@@ -96,27 +98,34 @@ class Image_Processor(object):
 
 				length = math.sqrt((Gx * Gx) + (Gy * Gy))	# calculate the length of the gradient
 
-				length = length / 4328 * 255				# normalise the length of gradient to the range 0 to 255
+				length = (length / 4328) * 255				# normalise the length of gradient to the range 0 to 255
 
 				length = 255 - int(length)					# convert the length to an integer and invert
-				
+				BGT = 0
 				if (length <= BGT):							# if the intensity is below a threshhold, set to black and update number of crack pixels
 					length = 0
 					crack_px += 1
 				
 				pixels_edge[x, y] = length, length, length	# draw the length in the edge image
 		
-		if (image == self.image):							# if the img was the class local, set the local outputs
+		if (image == self.img):							# if the img was the class local, set the local outputs
 			self.img_edge = image_edge
 			self.pix_edge = pixels_edge
 			
-			if (((crack_px * 1000) / total_px) >= crack_thresh): # If more than threshhold amount of black (edges), set cracks fround to True
+			if (((crack_px * 100) / total_px) >= crack_thresh): # If more than threshhold amount of black (edges), set cracks fround to True
 				self.crack_found = True
-		
-		return pixels_edge
+		self.verb_prnt("Crack amnt:" + str(((crack_px * 100) / total_px)))
+		if (self.verbose):
+                        self.img_edge.save("edge.bmp")
+		return image_edge
 	
 	# runs edge detection and gets whether a crack was found
-	def crack_det(self, image_path, BGT=245, crack_thresh=15):
+	def crack_det(self, image_path, BGT=245, crack_thresh=5):
+                self.verb_prnt("Image loaded!")
 		self.set_img(image_path)
-		self.edge_det(self.img, BGT, crack_thresh)
+		self.edge_det(BGT, crack_thresh)
 		return self.crack_found
+
+        def verb_prnt(self, mesg):
+                if (self.verbose):
+                        print mesg
