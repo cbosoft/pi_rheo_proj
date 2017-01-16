d4f610889bb0ab91c33205e8d3f0ed4af7f1e6d9
Chris <kfb12157@uni.strath.ac.uk>
2017-01-16 13:55:06 +0000

    added symbols to figure script

--- write_up/figures/shear_behav.py
+++ write_up/figures/shear_behav.py
@@ -1,7 +1,9 @@
 import matplotlib.pyplot as plt
-import numpy as np
+# import numpy as np
+
 #data read from figure 6 {figshearthin}
-x1 = [10000, 25000, 31000, 40000, 50000, 70000, 88000, 100000, 130000, 160000, 200000, 250000, 300000]
+x1 = [10000, 25000, 31000, 40000, 50000, 70000, 88000, 100000, 130000, 160000,
+200000, 250000, 300000]
 y1 = [3800, 2900, 2600, 2200, 1900, 1400, 1050, 780, 550, 400, 280, 195, 45]
 
 #data read from figure on second page {figshearthick}
@@ -15,19 +17,22 @@ ax1 = f.add_subplot(121)
 ax2 = f.add_subplot(122)
 
 #plot 1
-ax1.set_title("Shear Thinning")
+ax1.set_title(r"$\mathrm{Shear\ Thinning}$")
 ax1.plot(x1, y1)
 ax1.set_xscale("log")
 ax1.set_yscale("log")
 
 #plot 2
-ax2.set_title("Shear Thickening")
+ax2.set_title(r"$\mathrm{Shear\ Thickening}$")
 ax2.plot(x2, y2)
 ax2.set_xscale("log")
 ax2.set_yscale("log")
 
 # Set common labels
-f.text(0.5, 0.04, 'Shear Stress (Pa)', ha='center', va='center')
-f.text(0.06, 0.5, 'Viscosity (Pa.s)', ha='center', va='center', rotation='vertical')
+f.text(0.5, 0.04, r'$\mathrm{Shear\ Stress},\ \sigma\ (Pa)$', ha='center',
+va='center')
+f.text(0.06, 0.5, r'$\mathrm{Viscosity},\ \mu\ (Pa.s)$', ha='center',
+va='center',
+rotation='vertical')
 plt.savefig("./figures/fig_shear_behav.png")
 
