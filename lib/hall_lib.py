244348d27366cbbdaf82e5e0ff56f3790deeed0c
Chris <kfb12157@uni.strath.ac.uk>
2017-01-17 14:15:12 +0000

    records speed

--- lib/hall_lib.py
+++ lib/hall_lib.py
@@ -8,24 +8,27 @@
 import time
 import thread as td
 import RPi.GPIO as gpio
+import sys
 
 hall_pin = 16  # GPIO23
 
-def incr():
-    global counter
-    counter += 1
+def incr(channel):
+	global counter
+	counter += 1
 
 # set up pins
 gpio.setmode(gpio.BOARD)
 gpio.setup(hall_pin, gpio.IN, pull_up_down = gpio.PUD_UP)
 counter = 0
 gpio.add_event_detect(hall_pin, gpio.RISING, callback=incr)
-
+time_period = 0.1
+magnet_count = 2
 
 try:
 	while (True):
-	        print "Speed:" + str(float(counter / 6)) 
-            counter = 0
-        	time.sleep(0.1)
+		sys.stdout.write('\r Speed (RPM):' + (str(float(counter * float(60 / (time_period * magnet_count)))) + ("").zfill(4))[:4])
+		sys.stdout.flush()
+		counter = 0
+        	time.sleep(time_period)
 except KeyboardInterrupt:
 	gpio.cleanup()
