f883a57ca7e0f2a20cf8c8d3ffdc652daca5dfe9
Chris <kfb12157@uni.strath.ac.uk>
2017-01-17 13:17:12 +0000

    fixed

--- lib/hall_lib.py
+++ lib/hall_lib.py
@@ -12,12 +12,12 @@ import RPi.GPIO as gpio
 hall_pin = 16  # GPIO23
 
 # set up pins
-gpio.setmode(GPIO.BOARD)
-gpio.setup(hall_pin, GPIO.IN)
+gpio.setmode(gpio.BOARD)
+gpio.setup(hall_pin, gpio.IN, pull_up_down = gpio.PUD_UP)
 
 try:
 	while (True):
-        print str(GPIO.input(hall_pin))
-        time.sleep(0.5)
-	except KeyboardInterrupt:  # When 'Ctrl+C' is pressed, the child program destroy() will be  executed.
-		gpio.cleanup()
+	        print str(gpio.input(hall_pin)) 
+        	time.sleep(0.5)
+except KeyboardInterrupt:
+	gpio.cleanup()
