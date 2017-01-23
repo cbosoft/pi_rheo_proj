ad60c00e6c10d61ebc2774eb2e46627ccada4a99
cbosoft <kfb12157@uni.strath.ac.uk>
2017-01-23 18:24:13 +0000

    begun writing autotune method

--- lib/control.py
+++ lib/control.py
@@ -89,3 +89,91 @@ class PIDcontroller(object):
         if (self.D_On):
             self.ca[2] += (self.KD / self.sample_time) * (self.e[2] - (2 * self.e[1]) + self.e[0])
         return self.ca[2]
+    
+    def tune(self, time_constant, steady_state_gain, steps=20, maxs=5):
+        #for each possible tuning arrangement...
+        #gets response to a unit step at t=0
+        
+        #get list of total number of maximums
+        
+        #check responses for quickest rise with non-zero number of maximums (but less than 5)
+        
+        current_tuning = (self.KP, self.KI, self.KP)
+        
+        points = [[[0, 0], [0, 0]]] * 2 # x, y
+        maxima_count = [0] * 2
+        first_xs = [0] * 2
+        for sP in range(0, maxs):
+            self.KP = 0 + sP * (maxs / steps)
+            for sI in range(0, maxs):
+                self.KI = 0 + sP * (maxs / steps)
+                for sD in range(0, maxs):
+                    self.KD = 0 + sP * (maxs / steps)
+                    this_points = self.do_sim(time_constant, steady_state_gain)
+                    points.append(this_points)
+                    (a, b) = self.count_maxima(this_points)
+                    maxima_count.append(a)
+                    first_xs.append(b)
+                        
+    def do_sim(self, time_constant, steady_state_gain):
+        self.set_point = 1
+        resp = [[0.0, 0.0], [0.0, 0.0]]
+        y = [0.0, 0.0]
+        ca = [0.0, 0.0]
+        for i in range(0, 100):
+            y[0] = y[1]
+            ca[0] = ca[1]
+            y[1] = ((time_constant - self.sample_time) / time_constant) * y[0] + ((steady_state_gain * self.sample_time) / time_constant) * ca[0]
+            ca[1] = self.get_control_action(y[1])
+            resp.append([i, y[1]])
+            print y[1]
+        return resp
+    
+    def count_maxima(self, points):
+        gradient = 0
+        last_gradient = 0
+        max_cnt = 0
+        first_max_x = 0.0
+        last_y = points[0][1]
+        maxima_count = 0
+        
+        for i in range(0,len(points)):
+            if points[i][1] > last_y:
+                gradient = 1
+            elif points[i][1] == last_y:
+                gradient = 0
+            else:
+                gradient = -1
+                
+            if ((gradient < 0) and (last_gradient >= 0)):
+                maxima_count += 1
+                if (maxima_count == 1):
+                    first_max_x = points[i][0]
+                    
+            last_gradient = gradient
+            last_y = points[i][1]
+        return [maxima_count, first_max_x]
+        
+        
+if __name__ == "__main__":
+    a = PIDcontroller()
+    a.tune(2, 1)
+    #TEST AND REDO
+        
+        
+        
+        
+        
+        
+        
+        
+        
+        
+        
+        
+        
+        
+        
+        
+        
+        
