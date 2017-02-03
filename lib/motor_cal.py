b94480e5acfaed6173c29b154b0bdb9135853328
Chris <kfb12157@uni.strath.ac.uk>
2017-02-03 13:05:15 +0000

    separated plot script to new file

--- lib/motor_cal.py
+++ lib/motor_cal.py
@@ -30,23 +30,6 @@ try:
     print "Output saved as \"spdvals.csv\""
     mot.clean_exit()
     outp.close()
-    datf = open("./spdvals.csv",r")
-    datl = datf.readlines()
-    datf.close()
-    vals = [0] * len(datl)
-    speeds = [0] * len(datl)
-    for i in range(0, len(datl)):
-        splt = datl[i].split(",")
-        vals[i] = splt[0]
-        speeds[i] = splt[1]
-    f = plt.figure(figsize=(5, 5))
-    ax1 = f.add_subplot(111)
-    ax1.plot(vals, speeds)
-    ax1.set_xlabel(r'Potentiometer Value, unitless', ha='center',
-    va='center', fontsize=12)
-    ax1.set_ylabel(r'Measured Speed, RPM', ha='center',
-    va='center', fontsize=12)
-    plt.savefig("./mot_cal_val_vs_spd.png")
     
     
 except KeyboardInterrupt:
