1b7a8c2c02cb35939d80e318a871acd04d3e4b8c
Chris <kfb12157@uni.strath.ac.uk>
2017-02-15 16:08:12 +0000

    lotsa data!

---
+++ lib/interval_testing/plot_timings_hist.py
@@ -0,0 +1,109 @@
+#
+# From a list of responses, gets time span between responses and forms a histogram
+#
+import matplotlib.pyplot as plt
+import numpy as np
+from glob import glob
+
+def histo(log_path, out_fig, xlabel, ylabel, colour="blue"):
+    #Read logfile (light sensor)
+    datf = open(log_path, "r")
+    datl = datf.readlines()
+    datf.close()
+
+    #maximum number of lines to process from data (for looking at specific sections)
+    maxlen = 1000
+    lento = len(datl)
+    if (maxlen < lento):
+        lento = maxlen
+
+    
+    # Create lists
+    t = [0] * lento  # Time
+    st = [0] * lento  # Specific time (time from start of run, not epoch)
+    dt = [0] * lento  # Time between measurements (should be fairly constant)
+
+    # Split data into lists for easy use
+    for i in range(0, lento):
+        splt = datl[i].split(",", 5)
+        t[i] = float(splt[0])
+
+        if (i == 0):
+            st[i] = 0
+            dt[i] = 0
+        else:
+            st[i] = t[i] - t[0]
+            dt[i] = t[i] - t[i - 1]
+
+    # Plot Data
+    f = plt.figure(figsize=(10, 7.5))
+    ax1 = f.add_subplot(111)
+
+    # Bins might need tweaking between datasets
+    bins = np.linspace(-10, 40, 6)
+
+    (n, b, patches) = ax1.hist(dt, bins, color=colour)
+    
+    ax1.set_xlabel(xlabel, ha='center',
+    va='center', fontsize=12)
+    ax1.set_ylabel(ylabel, ha='center',
+    va='center', fontsize=12)
+    
+    plt.savefig(out_fig)
+    plt.close(f)
+    high = 0
+    popval = 0
+    tot = 0.01
+    for i in range(0, len(n)):
+        if n[i] > high:
+            high = n[i]
+            popval = b[i]
+        tot += n[i]
+    
+    return ((high / tot), (popval / 100))
+
+if __name__ == "__main__":
+    fracl = [0.0] * 0
+    maxl = [0.0] * 0
+    logno = [0.0] * 0
+    for p in sorted(glob(r"./light sensor/log_hall_*.csv")):
+        (frac, maxv) = histo(p, "light_" + p[24:-4] + ".png", r"$Interval, ms$", r"$Occurance$")
+        fracl.append(frac)
+        maxl.append(maxv)
+        logno.append(float(p[24:-4]))
+    
+    # Plot (meta) Data
+    f = plt.figure(figsize=(10, 10))
+    ax1 = f.add_subplot(111)
+    ax1.plot(logno, fracl)
+    ax1.scatter(logno, maxl, color="red")
+    #ax1.plot([logno[0], logno[0] + len(logno) - 1], [0.1, 0.1], color = "red")
+    ax1.set_xlabel("$File\ number$", ha='center',
+    va='center', fontsize=16)
+    ax1.set_ylabel("$Fraction\ of\ intervals\ on\ target$", ha='center',
+    va='center', fontsize=16)
+    
+    plt.savefig("meta_plot_light.png")
+
+    fracl = [0.0] * 0
+    maxl = [0.0] * 0
+    logno = [0.0] * 0
+    for p in sorted(glob(r"./hall effect/log_hall_*.csv")):
+        (frac, maxv) = histo(p, "hall_" + p[23:-4] + ".png", r"$Interval, ms$", r"$Occurance$")
+        fracl.append(frac)
+        maxl.append(maxv)
+        logno.append(float(p[23:-4]))
+    
+    print logno[0] + len(logno) - 1
+    # Plot (meta) Data
+    f = plt.figure(figsize=(10, 10))
+    ax1 = f.add_subplot(111)
+    ax1.plot(range(0, len(fracl)), fracl)
+    ax1.scatter(range(0, len(fracl)), maxl, color="red")
+    #ax1.plot([logno[0], logno[0] + len(logno) - 1], [0.1, 0.1], color = "red")
+    ax1.set_xlabel("$File\ number$", ha='center',
+    va='center', fontsize=16)
+    ax1.set_ylabel("$Fraction\ of\ intervals\ on\ target$", ha='center',
+    va='center', fontsize=16)
+    
+    plt.savefig("meta_plot_hall.png")
