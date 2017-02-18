d41ad869b6bc10f792c6fc9428e37863a27f4f3c
cbosoft <kfb12157@uni.strath.ac.uk>
2017-02-18 02:44:55 +0000

    roughly wrote up somme prelim tests

--- write_up/build.bat
+++ write_up/build.bat
@@ -1,6 +1,6 @@
 rem update wordcount
 T:\Python\python.exe .\updatewordcount.py
 rem get figures
-call .\getfigs.bat
+rem call .\getfigs.bat
 rem build pdf
 "C:\Program Files\MiKTeX 2.9\miktex\bin\x64\texify.exe" --pdf --synctex --clean ./pi_report.tex
