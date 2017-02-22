9fc2d823ac0738686ec67e8102bfbc9c442ed724
Chris <kfb12157@uni.strath.ac.uk>
2017-02-22 16:18:13 +0000

    updated log and report

--- write_up/figures/plot_speedvval.py
+++ write_up/figures/plot_speedvval.py
@@ -11,18 +11,19 @@ av_volt = [0] * 0
 av_spd = [0] * 0
 pv = [0] * 0
 
-for i in range(4, len(datl) - 2):
+for i in range(2, len(datl) - 2):
     splt = datl[i].split(",", 13)
     av_volt.append(float(splt[6]))
     av_spd.append(float(splt[12]))
     pv.append(float(splt[0]))
 
+# Speed v Value
 # Set up figure
 f = plt.figure(figsize=(7, 7))
 ax = f.add_subplot(111)
 
 # Calculate trendline
-z = np.polyfit(pv, av_spd, 1)
+z = np.polyfit(pv[2:], av_spd[2:], 1)
 tl = np.poly1d(z)
 
 # Plot data and trendline
@@ -37,3 +38,21 @@ plt.legend(loc=3)
 
 # Show plot
 plt.savefig("./figures/figspeedvval.png")
+plt.close(f)
+
+# Voltage v Value
+# Set up figure
+f = plt.figure(figsize=(7, 7))
+ax = f.add_subplot(111)
+
+# Plot data
+ax.plot(pv, av_spd, label="$Motor\ Voltage$")
+
+ax.set_xlabel("\n $Potentiometer\ Value,\ unitless$", ha='center', va='center', fontsize=24)
+ax.set_ylabel("$Average\ Supply\ Voltage,\ V$\n", ha='center', va='center', fontsize=24)
+
+plt.legend(loc=3)
+
+
+# Show plot
+plt.savefig("./figures/figvoltvval.png")
