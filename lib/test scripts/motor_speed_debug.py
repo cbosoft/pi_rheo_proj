f4c03ed72ca4f8c84905a0c78b6d5305f8ae1d7a
Chris <kfb12157@uni.strath.ac.uk>
2017-02-28 18:11:29 +0000

    updated script

--- lib/test scripts/motor_speed_debug.py
+++ lib/test scripts/motor_speed_debug.py
@@ -1,22 +1,20 @@
 #
 # Motor Speed Test Script
 #
-# Will set the digpot value and hold for a minute before moving on to the next - Small sweep from 48 to 128, step 8, hold 20s
+# Will set the digpot value and hold for some time before moving on to the next
 #
+
 import sys
 sys.path.append('./..')
 
 import motlib
 import time
-import matplotlib.pyplot as plt
 
-hall_pin = 16
-mag_count = 1
-mot = motlib.motor(startnow=True, poll_logging=True, log_dir="./speed_test_new")
+mot = motlib.motor(startnow=True, poll_logging=True, log_dir="./current_test_tues")
 
 print "Starting..."
 mot.pot.set_resistance(48)
-time.sleep(2)
+time.sleep(0.5)
 try:
     for i in range(0, 17):
         print "Setting potval to " + str(0 + (i * 8))
