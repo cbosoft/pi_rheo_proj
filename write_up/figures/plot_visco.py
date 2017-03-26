ca3f9f1e4ca85c2bf663b97dc24c6543e0f142dd
Chris <kfb12157@uni.strath.ac.uk>
2017-03-26 19:16:09 +0100

    updated to give two proper visosity comparison graphs

--- write_up/figures/plot_visco.py
+++ write_up/figures/plot_visco.py
@@ -32,6 +32,7 @@ T = [0] * 0
 tau = [0] * 0
 gam_dot = [0] * 0
 mu = [0] * 0
+s2t = [0] * 0
 
 roo = 0.044151 / 2.0  # outer cell outer radius in m
 ro = 0.039111 / 2.0  # outer cell radius in m
@@ -51,49 +52,69 @@ for i in range(1, len(datl)):
     pv.append(int(splt[3]))
 
 if True:
-    dr = filter(st, dr)
-    cr = filter(st, cr)
+    dr = filter(st, dr, method="butter",A=2, B=0.01)
+    cr = filter(st, cr, method="butter",A=2, B=0.01)
 
-for i in range(1, len(datl)):
+for i in range(100000, len(datl)):
+    s2t.append(st[i-1])
     cu.append((cr[i-1] * 3.58) - 7.784)
-    sp_rpms.append((dr[i-1] * 317.666) - 146.947)
-    sp_rads.append(sp_rpms[i - 1] / (2 * np.pi * 60))
-    sn_rpms.append((4.292 * pv[i - 1]) + 144.927)
+    sp_rpms.append((dr[i-1] * 312.809) - 159.196)
+    sp_rads.append((sp_rpms[-1] * 2 * np.pi) / 60)
+    sn_rpms.append(((5.13 * pv[i - 1]) + 15.275))
+    #    sn_rpms.append((4.292 * pv[i - 1]) + 144.927)
 
-    Ts.append((0.169 * cu[i - 1]) + 0.038)
-    T.append(Ts[i - 1] * (1 - ((sp_rads[i - 1] * 2 * np.pi * 60) / (4.292 * pv[i - 1] + 144.927))))
+    Ts.append((0.14 * cu[-1]) + 0.032)
+    #Ts.append(0.15)
+    T.append(Ts[-1] * (1 - (sp_rpms[-1]/ sn_rpms[-1])))
 
-    tau.append(T[i - 1] / (2 * np.pi * ri * ri * L))
+    tau.append(T[-1] / (2 * np.pi * ri * ri * L))
     
-    gam_dot.append(sp_rads[i - 1] * ri / (ro - ri))
+    gam_dot.append(sp_rads[-1] * (ri / (ro - ri)))
     
-    mu.append(tau[i - 1] / gam_dot[i - 1])
+    mu.append(tau[-1] / gam_dot[-1])
 
 if True:
-    mu = filter(st, mu, method="butter",A=10, B=0.08)
+    mu = filter(st, mu, method="butter", A=2, B=0.01)
 
+exp_visc = [0] * 2
+exp_t = [190] * 1
+exp_t.append(st[-1])
+exp_tau = [0] * 0
+exp_tau.append(tau[0])
+exp_tau.append(tau[-1])
 
 # Set up figure
-f = plt.figure(figsize=(7, 7))
+f = plt.figure(figsize=(8, 8))
 ax = f.add_subplot(111)
-plt.title("Placeholder graph!")
+
 # Plot data and trendline
-ax.plot(st[150000:], mu[150000:], 'b')
-
-#ax2 = ax.twinx()
-#ax2.plot(st, T, 'g')
-
-#ax3 = ax2.twinx()
-#ax3.plot(st, Ts, 'r')
-
-#ax4 = ax3.twinx()
-#ax4.plot(st, cu, 'o')
-
+ax.plot(s2t[0::100], mu[0::100], 'b.')
+ax.plot(exp_t, exp_visc, 'g--')
+#ax.set_ylim([-10, 10])
+#ax.set_yscale("log")
 ax.set_xlabel("\n $Time,\ s$", ha='center', va='center', fontsize=24)
 ax.set_ylabel("$Viscosity,\ Pa.s$\n", ha='center', va='center', fontsize=24)
 
 
 # Show plot
 plt.grid(which='both', axis='both')
-plt.savefig("./visco.png")
+plt.savefig("./fig_visco_v_t.png")
+plt.close(f)
+
+# Set up figure
+f = plt.figure(figsize=(8, 8))
+ax = f.add_subplot(111)
+
+# Plot data and trendline
+ax.plot(tau[0::100], mu[0::100], 'b.')
+ax.plot(exp_tau, exp_visc, 'g--')
+#ax.set_ylim([-10, 10])
+#ax.set_yscale("log")
+ax.set_xlabel("\n $Shear\ Stress,\ Pa$", ha='center', va='center', fontsize=24)
+ax.set_ylabel("$Viscosity,\ Pa.s$\n", ha='center', va='center', fontsize=24)
+
+
+# Show plot
+plt.grid(which='both', axis='both')
+plt.savefig("./fig_visco_v_tau.png")
 
