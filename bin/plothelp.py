904547cc6890b7a6db9e9a4bea1a024378d876bd
cbosoft <kfb12157@uni.strath.ac.uk>
2017-04-10 07:52:33 +0100

    added script for getting a fit for data

---
+++ bin/plothelp.py
@@ -0,0 +1,26 @@
+# plot helper
+# useful defs for plotting
+import numpy as np
+
+def fit_line(x, y, dg, x_name="x", y_name="y"):
+    x = np.array(x)
+    y = np.array(y)
+    coeffs = np.polyfit(x, y, dg)
+    fit = 0
+    fit_eqn = "$fit:\ {} =".format(y_name)
+    cf_str = ""
+    for i in range(0, len(coeffs)):
+        fit += coeffs[i] * (x ** (len(coeffs) - 1 - i))
+        if coeffs[i] < 0:
+            cf_str = "{:.9f}".format(coeffs[i])
+        else:
+            cf_str = "+{:.9f}".format(coeffs[i])
+        if (len(coeffs) - 1 - i) > 1:
+            fit_eqn += " {} \\times {}^{}".format(cf_str, x_name, "{" + str((len(coeffs) - 1 - i)) + "}")
+        elif (len(coeffs) - 1 - i) == 1:
+            fit_eqn += " {} \\times {}".format(cf_str, x_name)
+        else:
+            fit_eqn += " {}".format(cf_str)
+
+    fit_eqn += "$"
+    return fit, fit_eqn, coeffs
