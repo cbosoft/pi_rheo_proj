fixed style, added standard deviation errorbars

write_up/figures/plot_fv.py
@@ -15,10 +15,9 @@ from plothelp import fit_line
 def fitf(x, a, b):
     return a * x[0] + b + 0.0156 * x[1]
 
-def fit2f(x, a, b, c):
-    return a * x[0] + b + c * x[1]
+def fit2f(x, a, b):
+    return a * (x[0]) + b
 
-######################## Calibrate using "Pure" glycerol
 # Geometry of the couette cell
 
 roo = 0.044151 / 2.0  # outer cell outer radius in m
@@ -34,104 +33,143 @@ dxsa = dxsa * 1000 # ml / m
 fill_height = 15 / dxsa
 
 # Reads the data
-datf = pd.read_csv("./../../logs/glycerol_long_sweep.csv")
+datf = pd.read_csv("./../../logs/g9873c.csv")
 
-stg = datf['t']
-stg = stg - stg[0]
-dr = datf['dr']
-cr = datf['cr']
-pv = datf['pv']
+stg     = datf['t']
+stg     = stg - stg[0]
+dr      = datf['dr']
+cr      = datf['cr']
+cr2a    = datf['cr2a']
+cr2b    = datf['cr2b']
+pv      = datf['pv']
 
 # Filter noise from data
 dr = filter(stg, dr, method="butter", A=2, B=0.0001)
 cr = filter(stg, cr, method="butter", A=2, B=0.0001)
+cr2a = filter(stg, cr2a, method="butter", A=2, B=0.0001)
+cr2b = filter(stg, cr2b, method="butter", A=2, B=0.0001)
 
 # Chop off unwanted data
-start = 200000
-stop = -1 #* (len(cr) - start - 180 * 1000)
-skip = 1
-stg = stg[start:stop:skip]
-dr = dr[start:stop:skip]
-cr = cr[start:stop:skip]
-pv = pv[start:stop:skip]
+start   = 20000
+stop    = -1 * (int(len(cr) * 1) - 25000)
+skip    = 1000
+stg     = stg[start:stop:skip]
+dr      = dr[start:stop:skip]
+cr      = cr[start:stop:skip]
+cr2a    = cr2a[start:stop:skip]
+cr2b    = cr2b[start:stop:skip]
+pv      = pv[start:stop:skip]
 
 # Calculate the required stall torque for these readings
-musg    = [1.328] * len(cr)
+musg    = [1.129] * len(cr)
 sp_rpms = dr * 316.451 - 163.091
 sp_rads = (sp_rpms * 2 * np.pi) / 60
 sn_rpms = 5.13 * pv + 15.275
 gam_dot = (sp_rads * ri) / (ro - ri)
 tau     = musg * gam_dot
 T       = tau * (2 * np.pi * ri * ri * fill_height) 
+
 Ts      = T / (1.0 - (sp_rpms / sn_rpms))
-cu      = (25.177 * cr) - 45.264
+cu      = 16.573 * cr - 29.778
+cu2a    = 11.307 * cr2a - 29.066
+cu2b    = 11.307 * cr2b - 29.066
 cub     = 0.00229473 * pv + 0.48960784
+cu      = (cu + cu2a + cu2b) / 3
 vo      = 0.0636 * pv + 2.423
 
 # Relate the stall torque to the current supply
-eff, __ =  curve_fit(fit2f, [(cu - cub), vo], T)
+eff, __ =  curve_fit(fit2f, [(cu - cub)], T)
 
 # Calculate the viscosity using the read data and the fit
-Tg_fc       = eff[0] * (cu - cub) + eff[1] + eff[2] * vo
+Tg_fc       = eff[0] * ((cu - cub)*vo) + eff[1]
 taug_fc     = Tg_fc / (2 * np.pi * ri * ri * fill_height)
 mug_fc = taug_fc / gam_dot
 
+f = plt.figure(figsize=(8, 8))
+ax = f.add_subplot(111)
+ax.errorbar((cu - cub), T, 
+    xerr=(0.05 * np.array(cu)), marker='o', linestyle='None', label="$Required\ Torque$")
+ax.plot((cu-cub), ((cu - cub) * eff[0] + eff[1]), label="$T\ =\ {:.3E} * \Delta I_{} + {:.3E}$".format(eff[0], "{ms}", eff[1]))
 
+ax.set_xlabel("\n $\Delta Current,\ A$", ha='center', va='center', fontsize=24)
+ax.set_ylabel("$Torque,\ Nm$\n", ha='center', va='center', fontsize=24)
+#plt.legend(loc=4)
+plt.grid(which='both', axis='both')
+plt.savefig("./fig_t_cal.png")
 
-########### Check the calibration using the water run
-# Reads the data
-datf    = pd.read_csv("./../../logs/water_long_sweep.csv")
+def check(filename, viscosity, fillvol):
+    ########### Check the calibration using the water run
+    # Reads the data
 
-stw      = datf['t']
-stw      = stw - stw[0]
-dr      = datf['dr']
-cr      = datf['cr']
-pv      = datf['pv']
+    # Geometry of the couette cell
 
-# Filter noise from data
-dr      = filter(stw, dr, method="butter", A=2, B=0.001)
-cr      = filter(stw, cr, method="butter", A=2, B=0.001)
+    roo = 0.044151 / 2.0  # outer cell outer radius in m
+    ro = 0.039111 / 2.0  # outer cell radius in m
+    ri = 0.01525  # inner cell radius in m
 
-# Chop off unwanted data
-start   = 200000
-stop    = -1 #* (len(cr) - start - 180 * 1000)
-skip    = 1
-stw      = stw[start:stop:skip]
-dr      = dr[start:stop:skip]
-cr      = cr[start:stop:skip]
-pv      = pv[start:stop:skip]
+    icxsa = np.pi * (ri ** 2)
+    ocxsa = np.pi * (ro ** 2)
+    dxsa = ocxsa - icxsa  # vol per height in m3/m
+    dxsa = dxsa * 1000 # l / m
+    dxsa = dxsa * 1000 # ml / m
 
-# Calculate viscosity
-musw    = [0.001005] * len(cr)
-cu      = (25.177 * cr) - 45.264
-cub     = 0.00229473 * pv + 0.48960784
-sp_rpms = dr * 316.451 - 163.091
-sp_rads = (sp_rpms * 2 * np.pi) / 60
-sn_rpms = 5.13 * pv + 15.275
-vo      = 0.0636 * pv + 2.423
+    fill_height = fillvol / dxsa
 
-#T calibration
-gam_dotw = (sp_rads * ri) / (ro - ri)
-Tw_fc       = eff[0] * (cu - cub) + eff[1] + eff[2] * vo
-tauw_fc     = Tw_fc / (2 * np.pi * ri * ri * fill_height) 
-muw_fc = tauw_fc / gam_dotw
+    datf = pd.read_csv(filename)
 
-# Compare with Lab Rheom
-mur = [0] * 0
-gam_dotr = [0] * 0
+    stw     = datf['t']
+    stw     = stw - stw[0]
+    dr      = datf['dr']
+    cr      = datf['cr']
+    cr2a    = datf['cr2a']
+    cr2b    = datf['cr2b']
+    pv      = datf['pv']
 
-# Read csv
-datf = pd.read_csv("./../../logs/ref_fluid_rheom_res/g100.csv")
-mur.append(datf['mu'])
-gam_dotr.append(datf['gam_dot'])
+    # Filter noise from data
+    dr      = filter(stw, dr, method="butter", A=2, B=0.001)
+    cr      = filter(stw, cr, method="butter", A=2, B=0.001)
+    cr2a    = filter(stw, cr2a, method="butter", A=2, B=0.001)
+    cr2b    = filter(stw, cr2b, method="butter", A=2, B=0.001)
+
+    # Calculate viscosity
+    musw    = [viscosity] * len(cr)
+    cu      = 16.573 * cr - 29.778
+    cu2a    = 11.307 * cr2a - 29.066
+    cu2b    = 11.307 * cr2b - 29.066
+    cu      = np.array((cu + cu2a + cu2b) / 3)
+    cub     = 0.00229473 * pv + 0.48960784
+    sp_rpms = dr * 316.451 - 163.091
+    sp_rads = (sp_rpms * 2 * np.pi) / 60
+    sn_rpms = 5.13 * pv + 15.275
+    vo      = 0.0636 * pv + 2.423
+
+    #T calibration
+    gam_dotw    = (sp_rads * ri) / (ro - ri)
+    #Tw_fc       = eff[0] * (cu - cub) + eff[1]
+    Tw_fc       = eff[0] * ((cu - cub) *vo) + eff[1]
+    tauw_fc     = Tw_fc / (2 * np.pi * ri * ri * fill_height) 
+    muw_fc = tauw_fc / gam_dotw
+    return stw, muw_fc, musw
+
+st9873, mufc9873, mus9873 = check("./../../logs/g9873c.csv", 1.129, 15)
+st9623, mufc9623, mus9623 = check("./../../logs/g9623c.csv", 0.9061, 15)
+st9375, mufc9375, mus9375 = check("./../../logs/g9375c.csv", 0.6102, 15)
+st8887, mufc8887, mus8887 = check("./../../logs/g8887c.csv", 0.4005, 15)
 
 # Plot
-f = plt.figure(figsize=(10, 10))
+f = plt.figure(figsize=(8, 8))
 ax = f.add_subplot(111)
-ax.plot(stg, musg)
-ax.plot(stw, musw)
-ax.plot(stg, mug_fc)
-ax.plot(stw, muw_fc)
+start = 20000
+stop = -1 * (len(st9873) - 115000)
+skip = 1000
+ax.plot(stg, musg, 'b')
+ax.plot(stg, mug_fc, 'bx')
+
+ax.plot(st9375[start:stop:skip], mus9375[start:stop:skip], 'g')
+ax.plot(st9375[start:stop:skip], mufc9375[start:stop:skip], 'g^')
+
+ax.plot(st9623[start:stop:skip], mus9623[start:stop:skip], 'r')
+ax.plot(st9623[start:stop:skip], mufc9623[start:stop:skip], 'r^')
 ax.set_yscale('log')
 ax.set_xlabel("\n $Time,\ s$", ha='center', va='center', fontsize=24)
 ax.set_ylabel("$Viscosity,\ Pa.s$\n", ha='center', va='center', fontsize=24)

write_up/figures/plot_speed_v_rvolt.py
@@ -4,33 +4,18 @@ sys.path.append("./../../bin")
 from filter import filter
 import matplotlib.pyplot as plt
 import numpy as np
+import pandas as pd
 from glob import glob
 
 # Read csv
-#datf = open("./../../logs/pulley_test/smallx4.csv", "r")
-datf = open("./../../logs/long_cal.csv", "r")
-datl = datf.readlines()
-datf.close()
+datcsv = pd.read_csv("./../../logs/dual_hes_long100.csv")
+st = np.array(datcsv['t'])
+st = st - st[0]
+rv = np.array(datcsv['dr'])
+pv = np.array(datcsv['pv'])
 
-# Create lists for sorting
-
-rv = [0] * 0
-t = [0] * 0
-st = [0] * 0
-pv = [0] * 0
-
-splt = datl[1].split(",", 5)
-tz = float(splt[0])
-
-for i in range(1, len(datl)):
-    splt = datl[i].split(",", 5)
-    t.append(float(splt[0]))
-    st.append(float(splt[0]) - tz)
-
-    rv.append(float(splt[1]))
-    pv.append(int(splt[3]))
-
-rv = filter(st, rv, method="butter", A=2, B=0.0001)
+#filter readings
+#rv = filter(st, rv, method="butter", A=2, B=0.0001)
 
 # Read csv
 datf = open("./../../logs/voltvval.csv", "r")
@@ -53,34 +38,125 @@ for i in range(2, len(datl)):
 av_speed_long = [0] * 0
 
 
-#print pv
-
 cur_pv = pv[0]
 pv_indx = 0
 for i in range(0, len(pv)):
     for j in range(0, len(p2v)):
         if (pv[i] - p2v[j]) < 8 and (pv[i] - p2v[j]) >= 0:
-            #print pv[i] - p2v[j]
+
             pv_indx = j
     av_speed_long.append(av_spd[pv_indx])
 
-tot_rvs = [[0] * 0] * len(p2v)
-print tot_rvs
-av_rvs = np.array([0] * len(p2v))
+rv_t_0 = [0] * 0
+rv_t_8 = [0] * 0
+rv_t_16 = [0] * 0
+rv_t_24 = [0] * 0
+rv_t_32 = [0] * 0
+rv_t_40 = [0] * 0
+rv_t_48 = [0] * 0
+rv_t_56 = [0] * 0
+rv_t_64 = [0] * 0
+rv_t_72 = [0] * 0
+rv_t_80 = [0] * 0
+rv_t_88 = [0] * 0
+rv_t_96 = [0] * 0
+rv_t_104 = [0] * 0
+rv_t_112 = [0] * 0
+rv_t_120 = [0] * 0
+rv_t_128 = [0] * 0
+
+av_rvs = np.array([0.0] * len(p2v))
 for i in range(0, len(pv)):
     for j in range(0, len(p2v)):
         if pv[i] == p2v[j]:
-            print tot_rvs[j], j
-            tot_rvs[j].append(rv[i])
-            #print tot_rvs, j, rv[i]
-            r = raw_input()
-print min(tot_rvs[4]), max(tot_rvs[4])
+            if j == 0 : rv_t_0.append(rv[i])
+            if j == 1 : rv_t_8.append(rv[i])
+            if j == 2 : rv_t_16.append(rv[i])
+            if j == 3 : rv_t_24.append(rv[i])
+            if j == 4 : rv_t_32.append(rv[i])
+            if j == 5 : rv_t_40.append(rv[i])
+            if j == 6 : rv_t_48.append(rv[i])
+            if j == 7 : rv_t_56.append(rv[i])
+            if j == 8 : rv_t_64.append(rv[i])
+            if j == 9 : rv_t_72.append(rv[i])
+            if j == 10 : rv_t_80.append(rv[i])
+            if j == 11 : rv_t_88.append(rv[i])
+            if j == 12 : rv_t_96.append(rv[i])
+            if j == 13 : rv_t_104.append(rv[i])
+            if j == 14 : rv_t_112.append(rv[i])
+            if j == 15 : rv_t_120.append(rv[i])
+            if j == 16 : rv_t_128.append(rv[i])
 
+# 2d list not working as expected, using manual (long hand) method :@
 stdv = [0] * 0
-for i in range(0, len(tot_rvs)):
-    stdv.append(np.std(tot_rvs[i]))
-    av_rvs[i] = np.average(tot_rvs[i])
 
+# pv = 0
+stdv.append(np.std(rv_t_0))
+av_rvs[0] = np.average(rv_t_0)
+
+# pv = 8
+stdv.append(np.std(rv_t_8))
+av_rvs[1] = np.average(rv_t_8)
+
+# pv = 16
+stdv.append(np.std(rv_t_16))
+av_rvs[2] = np.average(rv_t_16)
+
+# pv = 24
+stdv.append(np.std(rv_t_24))
+av_rvs[3] = np.average(rv_t_24)
+
+# pv = 32
+stdv.append(np.std(rv_t_32))
+av_rvs[4] = np.average(rv_t_32)
+
+# pv = 40
+stdv.append(np.std(rv_t_40))
+av_rvs[5] = np.average(rv_t_40)
+
+# pv = 48
+stdv.append(np.std(rv_t_48))
+av_rvs[6] = np.average(rv_t_48)
+
+# pv = 56
+stdv.append(np.std(rv_t_56))
+av_rvs[7] = np.average(rv_t_56)
+
+# pv = 64
+stdv.append(np.std(rv_t_64))
+av_rvs[8] = np.average(rv_t_64)
+
+# pv = 72
+stdv.append(np.std(rv_t_72))
+av_rvs[9] = np.average(rv_t_72)
+
+# pv = 80
+stdv.append(np.std(rv_t_80))
+av_rvs[10] = np.average(rv_t_80)
+
+# pv = 88
+stdv.append(np.std(rv_t_88))
+av_rvs[11] = np.average(rv_t_88)
+
+# pv = 96
+stdv.append(np.std(rv_t_96))
+av_rvs[12] = np.average(rv_t_96)
+
+# pv = 104
+stdv.append(np.std(rv_t_104))
+av_rvs[13] = np.average(rv_t_104)
+
+# pv = 112
+stdv.append(np.std(rv_t_112))
+av_rvs[14] = np.average(rv_t_112)
+
+# pv = 120
+stdv.append(np.std(rv_t_120))
+av_rvs[15] = np.average(rv_t_120)
+
+# pv = 128
+stdv.append(np.std(rv_t_128))
+av_rvs[16] = np.average(rv_t_128)
     
 # 1st Trend: speed as a function of potval
 zavspdpv = np.polyfit(p2v[4:], av_spd[4:], 1)
@@ -101,15 +177,15 @@ t3l = np.poly1d(z3z)
 
 # Plot data and trendline
 #ax.plot(rv, av_speed_long, 'o', label="$Recorded\ Speed$")
-#ax.plot(tl(pv), t3l(tl(pv)), 'r--', label="$v_{2} = {0:.3f} V_D {1:.3f}$".format(z3z[0], z3z[1], "{NL}"))
+ax.plot(tl(pv), t3l(tl(pv)), 'r-', label="$v_{2} = {0:.3f} V_D {1:.3f}$".format(z3z[0], z3z[1], "{m}"))
 
-ax.errorbar(av_rvs, av_spd, yerr=std, xerr=stdv, linestyle='None', marker='o')
+ax.errorbar(av_rvs, av_spd, yerr=std, xerr=stdv, linestyle='None', marker='o', label="$Logged Data$")
 
-ax.set_xlabel("\n $Read\ Voltage,\ V$", ha='center', va='center', fontsize=24)
-ax.set_ylabel("$Speed,\ RPM$\n", ha='center', va='center', fontsize=24)
+ax.set_xlabel("\n $Dynamo\ Voltage,\ V$", ha='center', va='center', fontsize=24)
+ax.set_ylabel("$Motor\ Speed,\ RPM$\n", ha='center', va='center', fontsize=24)
 
 # Show Legend
-#plt.legend()
+plt.legend(loc=4)
 
 # Show plot
 plt.grid(which='both', axis='both')

write_up/figures/plot_speed_v_val.py
@@ -48,7 +48,7 @@ for i in range(2, len(datl) - 2):
     read_speed.append(316.451 * rv - 162.091)
     read_pv.append(float(splt[3]))
 
-filtered_reading = filter(read_pv, read_speed)
+filtered_reading = filter(read_pv, read_speed, method="butter", A=2, B=0.0001)
 
 # Speed v Value
 # Set up figure
@@ -64,7 +64,8 @@ tl = np.poly1d(z)
 #plt.errorbar(pv, av_spd, yerr=av_spd_err, label="$Actual\ Motor\ Speed$", fmt='bx-', ecolor='g')
 #ax.plot(read_pv, read_speed, 'go', label='$Read\ Motor\ Speed$')
 
-ax.plot(read_pv, filtered_reading, '.', color=(1,0.5,0.5,1), label="$Filtered\ Speed\ Reading$")
+#ax.plot(read_pv, filtered_reading, '.', color=(1,0.5,0.5,1), label="$Filtered\ Speed\ Reading$")
+ax.errorbar(read_pv[::50000], filtered_reading[::50000], yerr=np.std(read_speed), marker='o', linestyle='None')
 ax.plot(pv, av_spd, 'b-', label="$Actual\ Motor\ Speed$")
 ax.plot(pv, tl(pv), 'g--', label="$v_{2} = {0:.3f}pv + {1:.3f}$".format(z[0], z[1], "{NL}"))
 

write_up/figures/plot_stall_torque_v_supply_current.py
@@ -58,7 +58,8 @@ fit_eqn += "$"
 fit, fit_eqn, __ = fit_line(cu, st, 1, "I_{ms}", "Ts")
 
 # Plot data and trendline
-ax.plot(cu, st, 'o', label="$Read\ Data$")
+ststd = np.std(st)
+ax.errorbar(cu, st, yerr=ststd, marker='o', linestyle='None', label="$Read\ Data$")
 ax.plot(cu, fit, 'r--', label=fit_eqn)
 
 ax.set_xlabel("\n $Current\ Supply,\ A$", ha='center', va='center', fontsize=24)