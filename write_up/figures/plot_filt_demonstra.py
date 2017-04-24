f0143ca02eafd3dcb45b45262e9dab5dd17baf6f
cbosoft <kfb12157@uni.strath.ac.uk>
2017-04-24 03:16:22 +0100

    slightly updated plots

--- write_up/figures/plot_filt_demonstra.py
+++ write_up/figures/plot_filt_demonstra.py
@@ -14,11 +14,11 @@ def filterff(path_file):
     datf = pd.read_csv(path_file)
     
     st = np.array(datf['t'])
-    st = st - st[0]
+    st = st - float(st[0])
     
     dr = np.array(datf['dr'])
     
-    drf = filter(st, dr, method="butter", A=2, B=0.001)
+    drf = filter(st, dr, method="butter", A=2, B=0.0001)
     return st, dr, drf
 
 def get_ft(x, y, f):
@@ -34,6 +34,22 @@ def get_ft(x, y, f):
     ft = np.fft.rfft(f)
     ps_f = np.real(ft*np.conj(ft))*np.square(dt)  
     return w, ps_n, ps_f
+    
+def fftnoise(f):
+    f = np.array(f, dtype='complex')
+    Np = (len(f) - 1) // 2
+    phases = np.random.rand(Np) * 2 * np.pi
+    phases = np.cos(phases) + 1j * np.sin(phases)
+    f[1:Np+1] *= phases
+    f[-1:-1-Np:-1] = np.conj(f[1:Np+1])
+    return np.fft.ifft(f).real
+
+def band_limited_noise(min_freq, max_freq, samples=1024, samplerate=1):
+    freqs = np.abs(np.fft.fftfreq(samples, 1/samplerate))
+    f = np.zeros(samples)
+    idx = np.where(np.logical_and(freqs>=min_freq, freqs<=max_freq))[0]
+    f[idx] = 1
+    return fftnoise(f)
 
 if __name__ == "__main__":
     # test script for testing
@@ -43,7 +59,7 @@ if __name__ == "__main__":
 
     # load up some noisy data
     # normal run (1)
-    x1, y1, f1 = filterff("./../../logs/dual_hes_long100.csv")
+    x1, y1, f1 = filterff("./../../logs/std_sweep.csv")
     # intermittent load
     x2, y2, f2 = filterff("./../../logs/intermittent_load_short.csv")
     
@@ -57,7 +73,9 @@ if __name__ == "__main__":
     plt.title("$b)\ Intermittent\ Loading$", fontsize=24)
     
     # get spectra
-    w, ps_n, ps_f = get_ft(x1, y1, f1)
+    noise = band_limited_noise(10, 10, samples=len(y1))
+    y_noisy = y1 + noise
+    w, ps_n, ps_f = get_ft(x1, y_noisy, f1)
 
     # Plot data and trendline
     ax_tl.plot(x1, y1, 'b', color=(0,0,1,0.2))
@@ -66,8 +84,9 @@ if __name__ == "__main__":
     ax_tl.set_ylabel("$Speed,\ RPM$\n", ha='center', va='center', fontsize=24)
     
     ps_offs = 0
-    ax_tr.set_ylim([0, 500000])
+    #ax_tr.set_ylim([0, 500000])
     ax_tr.yaxis.set_major_formatter(mtick.FormatStrFormatter('%.1e'))
+    print x2
     ax_tr.plot(w[ps_offs:], ps_n[ps_offs:len(w)], 'b', color=(0,0,1,0.2))
     ax_tr.plot(w[ps_offs:], ps_f[ps_offs:len(w)], 'b')
     ax_tr.set_xlabel("\n $Frequency,\ Hz$", ha='center', va='center', fontsize=24)
