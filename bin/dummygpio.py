38772d0905ee72418e6e89fd590f5b18e3b7166b
cbosoft <kfb12157@uni.strath.ac.uk>
2017-06-07 14:58:14 +0100

    Added dummygpio.py
    Added pseudo-package for dummying GPIO package when debugging not on RPi

---
+++ bin/dummygpio.py
@@ -0,0 +1,22 @@
+#
+# dummygpio.py
+#
+# dummy gpio class
+# for debugging
+
+HIGH = True
+LOW = False
+BCM = 0
+BOARD = 0
+
+def output(channel, value):
+    pass
+    
+def input(channel)
+    return 0
+    
+def setmode(numbering_system):
+    pass
+    
+def setup(channel, direction):
+    pass
