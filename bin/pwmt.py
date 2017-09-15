74fb83b12da6a133c5c15f63a60d66903a96a88c
cbosoft <kfb12157@uni.strath.ac.uk>
2017-09-15 16:09:21 +0100

    opsens 0+1 working, opsens 2... not so much

--- bin/pwmt.py
+++ bin/pwmt.py
@@ -6,20 +6,21 @@ m.start_poll(name="test.csv", controlled=False)
 #m.update_setpoint(200.0)
 #m.set_pot(6)
 print "waiting..."
-m.set_dc(15) # approx 100
+m.set_dc(30) # approx 100
 sleep(10)
 #m.pidc.tuning = (0.1, 1.0, 0.0)
 #m.update_setpoint(100.0)
 #m.start_control()
 try:
-    for i in range(20, 201):
-        #dc = i / 2.0
-        #m.set_dc(11)
-        sleep(0.25)
-        f = m.f_speeds
-        r = m.r_speeds
-        print "r0: ", r[0], " f0: ", f[0], "r1: ", r[1], "f1: ", f[1], "r2: ", r[2], "f2: ", f[2], "dc: ", m.ldc
-        sleep(0.25)
+    for i in range(0, 9):
+        for j in range(0, 10):
+            dc = 10 + (i * 10)
+            m.set_dc(dc)
+            sleep(0.25)
+            f = m.f_speeds
+            r = m.r_speeds
+            print "r0: ", r[0], " f0: ", f[0], "r1: ", r[1], "f1: ", f[1], "r2: ", r[2], "f2: ", f[2], "dc: ", m.ldc
+            sleep(0.25)
 except KeyboardInterrupt:
     print "Cancelling.."
 except:
