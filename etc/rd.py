063c34022594448bc067642df0a92a093b6d2876
Chris B <kfb12157@uni.strath.ac.uk>
2017-09-27 19:42:00 +0100

    test script for testing

--- etc/rd.py
+++ etc/rd.py
@@ -2,15 +2,20 @@ from glob import glob
 from sys import path
 
 import numpy as np
+import matplotlib.pyplot as plt
 
 path.append("./../bin/")
 
 from plothelp import plot_fit
 from plothelp import read_logf
+from filter import filter
 import resx
 
 I_EMFs = list()
 T_MSs  = list()
+
+aliemfs = list()
+altms = list()
 
 ref_logs = glob("./../logs/mcal*calcd.csv")
 
@@ -22,23 +27,39 @@ for i in range(0, len(ref_logs)):
     
     # mcal_[name]_[viscosity]_pas_[date+time].csv
     viscosity = float(ref_logs[i].split('_')[2])
-    print "\tFluid: ", ref_logs[i].split('_')[1]
-    print "\tViscosity: ", viscosity
+    #print "\tFluid: ", ref_logs[i].split('_')[1]
+    #print "\tViscosity: ", viscosity
+    
+    ## filtering!
+    Vms = filter(st, Vms)
+    cra = filter(st, cra)
+    
+    
     I_MS = resx.get_current(cra)
     I_CO = resx.get_current_coil(Vms)
     I_EMF = [0.0] * len(I_MS)
     for j in range(0, len(I_MS)):
         I_EMF[j] = I_MS[j] - I_CO[j]
-    I_EMFs.append(np.average(I_EMF))
+    aliemfs.extend(I_EMF)
+    I_EMFs.append(np.mean(I_EMF))
             
-    stress = viscosity * np.average(gamma_dot)
+    stress = viscosity * gamma_dot
     torque = resx.get_torque(stress, 15)
-    print "\tStrain:   ", np.average(gamma_dot)
-    print "\tStress:   ", stress
-    print "\tTorque:   ", torque
-    print "\tI emf :   ", I_EMFs[-1]
-    T_MSs.append(torque)
+    #print "\tStrain:   ", np.average(gamma_dot)
+    #print "\tStress:   ", stress
+    #print "\tTorque:   ", torque
+    #print "\tI emf :   ", I_EMFs[-1]
+    altms.extend(torque)
+    T_MSs.append(np.mean(torque))
 
-__, f_eqn, mot_cal = plot_fit(I_EMFs, T_MSs, 1, x_name="Iemf", y_name="Tau")
-print "New fit:"
-print "\tTau = Iemf * {} + {}".format(mot_cal[0], mot_cal[1])
+#print T_MSs
+#print I_EMFs
+fit, f_eqn, mot_cal = plot_fit(I_EMFs, T_MSs, 1, x_name="Iemf", y_name="T")
+f = plt.figure()
+ax = f.add_subplot(111)
+ax.plot(aliemfs, altms, "x")
+ax.plot(I_EMFs, T_MSs, "o")
+ax.plot(I_EMFs, fit)
+plt.show()
+#print "New fit:"
+#print "\tT = Iemf * {} + {}".format(mot_cal[0], mot_cal[1])
