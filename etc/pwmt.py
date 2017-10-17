48ea7584dd30ede9947263d96b1526f3a5dca934
cbosoft <kfb12157@uni.strath.ac.uk>
2017-10-17 13:43:32 +0100

    tsft

--- etc/pwmt.py
+++ etc/pwmt.py
@@ -8,7 +8,7 @@ import numpy as np
 
 import resx
 
-m = motor.motor(i_poll_rate=0.01)
+m = motor.motor()
 m.start_poll(name="test.csv", controlled=False)
 #m.update_setpoint(200.0)
 #m.set_pot(6)
@@ -23,16 +23,15 @@ volts = list()
 try:
     for i in range(0, 9):
         for j in range(0, 10):
-            dc = 10 + (i * 10)
+            dc = 10 #+ (i * 10)
             m.set_dc(dc)
             sleep(0.25)
-            f = m.f_speeds
-            r = m.r_speeds
-            print "r0: ", r[0], " f0: ", f[0], "r1: ", r[1], "f1: ", f[1], "r2: ", r[2], "f2: ", f[2], "dc: ", m.ldc
-            current = resx.get_current(m.volts[2])
-            volt = m.volts[7] * 1.0
-            currents.append(current)
-            volts.append(volt)
+            s = m.speeds
+            print "r0: ", s[0], " f0: ", s[1], "r1: ", s[2], "f1: ", s[3], "r2: ", s[0], "f2: ", s[0], "dc: ", m.ldc
+            #current = resx.get_current(m.volts[2])
+            #volt = m.volts[7] * 1.0
+            #currents.append(current)
+            #volts.append(volt)
             #"cra: ", m.volts[1], "crb: ", m.volts[2], 
             #print "I: ", current, " V: ", volt, " i: ", i
             sleep(0.25)
